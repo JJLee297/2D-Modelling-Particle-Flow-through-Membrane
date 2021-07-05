28f63726eb0cffa62bdf9cb15d43dd4550e9978b
Justin Lee <45504969+JJLee297@users.noreply.github.com>
2021-07-05 15:40:26 -0400

    Update Source.cpp

--- SourceCode/Source.cpp
+++ SourceCode/Source.cpp
@@ -9,84 +9,6 @@
 
 using namespace std;
 
-/*
-closureTest() - given an initial point, closureTest traces all possible 'bridges' over the pore openning. 
-If a fully connected bridge exists, closureTest() returns true. Else, it returns false.
-Fully connected bridge: A bridge is considered fully connected if it completely (without any gaps) connects both sides 
-of the membrane surface. Diagonal connections are allowed. 
-*/
-
-bool closureTest(char arr[100][200], int x, int y) {
-	vector<int> check;
-	vector<int> check2;
-	check.push_back(y);
-	for (int i = x + 1; i < 100; ++i) {
-		for (int k = 0; k < check.size(); ++k) {
-			if (arr[i][check[k] - 1] == '*')
-				if (find(check2.begin(), check2.end(), check[k] - 1) == check2.end())
-					check2.push_back(check[k] - 1);
-			if (arr[i][check[k]] == '*')
-				if (find(check2.begin(), check2.end(), check[k]) == check2.end())
-					check2.push_back(check[k]);
-			if (arr[i][check[k] + 1] == '*')
-				if (find(check2.begin(), check2.end(), check[k] + 1) == check2.end())
-					check2.push_back(check[k] + 1);
-		}
-		if (check2.size() != 0) {
-			check = check2;
-			check2.clear();
-		}
-		else {
-			return false; 
-		}
-
-		// the check vector is always sorted, due to the design of the if statements. The following if checks if the lowest element is 
-		// directly on top of or to the left of the membrane wall. 
-		if ((i > 70 && arr[i][check[0] - 1] == 'M') || (i == 70 && arr[71][check[0]] == 'M')) {
-			return true;
-		}
-	}
-	return false;
-}
-
-/*  There can be multiple definitions of clogged. We will only consider a pore 'clogged' if there exists a connected
-	path from one side of the membrane over the pore to the other side of the membrane.
-	Closure Check Algorithm:
-		1. Mark each particle that is stuck directly on top of the membrane on the left side (y = 50, x < 29)
-			* Also mark any particle that is stuck on the left side of the pore (x = 29, y < 50)
-		2. For each of those particles, run closureTest()
-		3. If any closureTest() runs return true, then return true
-		4. else, return false
-*/
-bool checkForClosure(char arr[100][200]) {
-	vector<int> init;
-	int yvals = 0;
-	for (int y = 0; y < 50; ++y) {
-		if (arr[29][y] == '*') {
-			init.push_back(y);
-			++yvals;
-		}
-	}
-	for (int x = 28; x >= 0; --x) {
-		if (arr[x][50] == '*') {
-			init.push_back(x);
-		}
-	}
-	for (int i = 0; i < yvals; ++i) {
-		if (closureTest(arr, 29, init[i])) {
-			return true;
-		}
-	}
-	for (int i = yvals; i < init.size(); ++i) {
-		if (closureTest(arr, init[i], 50)) {
-			return true;
-		}
-	}
-	yvals = 0;
-	init.clear();
-	return false;
-}
-
 
 // accesses txt file and records final array data, with A and B
 void write(char arr[100][200], int A, int B) {
@@ -95,14 +17,23 @@ void write(char arr[100][200], int A, int B) {
 	if (textfile.is_open()) {
 		for (int y = 199; y >= 0; --y) {
 			for (int x = 0; x < 100; ++x) {
-				textfile << arr[x][y] << " ";
+				if (x == 0) {
+					textfile << setw(3) << setfill(' ') << y;
+				}
+				textfile << setw(3) << setfill(' ') << arr[x][y];
 			}
 			textfile << endl;
 		}
+		textfile << "   ";
+		for (int i = 0; i < 100; ++i) {
+			textfile << setw(3) << setfill(' ') << i;
+		}
+		textfile << endl;
 		textfile << "\n\nTotal Stuck Particles: " << A << "\nTotal Tested Particles: " << B << endl;
 	}
 	textfile.close();
 }
+
 
 
 // checks if a particles new position coincides with another stuck particle or membrane wall
@@ -117,10 +48,18 @@ bool checkIfStuck(char arr[100][200], Particle part) {
 void showfull(char arr[100][200]) {
 	for (int y = 199; y >= 0; --y) {
 		for (int x = 0; x < 100; ++x) {
-			cout << arr[x][y];
+			if (x == 0) {
+				cout << setw(3) << setfill(' ') << y;
+			}
+			cout << setw(3) << setfill(' ') << arr[x][y];
 		}
 		cout << endl;
 	}
+	cout << "   ";
+	for (int i = 0; i < 100; ++i) {
+		cout << setw(3) << setfill(' ') << i;
+	}
+	cout << endl;
 }
 
 int main() {
