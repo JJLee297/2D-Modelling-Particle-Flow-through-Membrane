451dda12b023c96d2d2def029c112191f8af5016
Justin Lee <45504969+JJLee297@users.noreply.github.com>
2021-07-19 17:54:31 -0400

    Create source.py

---
+++ pythonVisuals/source.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#manually fill out
+runs = 25
+xinter = 30
+yinter = 30
+  
+  
+print("Enter the entries in a single line (separated by space): ")
+  
+entries = list(map(int, input().split()))
+
+matrix = np.array(entries).reshape(200, 100)
+  
+plt.imshow(matrix, cmap='Greys', interpolation='nearest')
+# filename format = "(xintercept-yintercept)runs-porosity.png, e.g. (30-30)25por2100.png"
+plt.savefig('({}-{}){}-por{}.png'.format(xinter, yinter, runs, yinter*(100 - xinter)), dpi=300)
+plt.show()
