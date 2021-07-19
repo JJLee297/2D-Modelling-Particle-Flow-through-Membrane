be6c6b35c02370bbbfdf3c5a2007ba6c3c187e96
Justin Lee <45504969+JJLee297@users.noreply.github.com>
2021-07-18 23:08:35 -0400

    Update Source.cpp

--- SourceCode/Source.cpp
+++ SourceCode/Source.cpp
@@ -8,27 +8,25 @@
 #include <algorithm>
 #include <iterator>
 #include <iomanip>
+#include <string>
 
 using namespace std;
+using ll = long long;
 
 char arr[100][200];
+int agg[100][200];
+ll totalA;
+ll totalB;
 
-void write(int A, int B) {
+void write(int A, int B, string name) {
 	ofstream textfile;
-	textfile.open("LatticeData.txt", ofstream::out | ofstream::trunc);
+	textfile.open(name, ofstream::out | ofstream::trunc);
 	if (textfile.is_open()) {
 		for (int y = 199; y >= 0; --y) {
 			for (int x = 0; x < 100; ++x) {
-				if (x == 0) {
-					textfile << setw(3) << setfill(' ') << y;
-				}
-				textfile << setw(3) << setfill(' ') << arr[x][y];
+				textfile << agg[x][y] << " ";
 			}
 			textfile << endl;
-		}
-		textfile << "   ";
-		for (int i = 0; i < 100; ++i) {
-			textfile << setw(3) << setfill(' ') << i;
 		}
 		textfile << endl;
 		textfile << "\n\nTotal Stuck Particles: " << A << "\nTotal Tested Particles: " << B << endl;
@@ -53,6 +51,33 @@ void showfull() {
 	}
 }
 
+void showAgg() {
+	for (int y = 199; y >= 0; --y) {
+		for (int x = 0; x < 100; ++x) {
+			cout << agg[x][y];
+		}
+		cout << endl;
+	}
+}
+
+void setAgg() {
+	for (int x = 0; x < 100; ++x) {
+		for (int y = 0; y < 200; ++y) {
+			agg[x][y] = 0;
+		}
+	}
+}
+
+void aggregate() {
+	for (int x = 0; x < 100; ++x) {
+		for (int y = 0; y < 200; ++y) {
+			if (arr[x][y] == '*') {
+				agg[x][y]++;
+			}
+		}
+	}
+}
+
 void createMembrane(int xx, int yy) {
 	float yint, xint;
 	xint = (float)xx;
@@ -60,29 +85,32 @@ void createMembrane(int xx, int yy) {
 	float slope = -(yint / xint);
 	for (int x = 0; x < 100; x++) {
 		for (int y = 0; y < 200; ++y) {
-			if (y < (slope * x + yint) || y < -slope * (x - (99 - xint)))
+			if (y < (slope * x + yint)) {
 				arr[x][y] = 'M';
-			else
+				//arr2[x][y] = 1;
+			}
+			else if (y < -slope * (x - (99 - xint))) {
+				arr[x][y] = 'M';
+				//arr2[x][y] = 2; 
+			}
+			else {
 				arr[x][y] = ' ';
+				//arr2[x][y] = 0;
+			}
 		}
 	}
 }
 
-int main() {
-	createMembrane(40, 50);
-	showfull();
-	system("pause");
-	// sim
-
+void sim(int xinter, int yinter) {
+	createMembrane(xinter, yinter);
 	int A = 0;
 	int B = 0;
-	srand(time(NULL));
+	bool breakcounter = false;
 	while (1) {
 		int xpos = rand() % 100;
-		int ypos = 199;
+		int ypos = rand() % 50 + 150;
 		Particle part(xpos, ypos);
 		Particle part2 = part;
-
 		while (1) {
 			part.moveParticle();
 			if (part.getHeight() == 0) {
@@ -90,7 +118,9 @@ int main() {
 				break;
 			}
 			else if (checkIfStuck(part) == true) {
-				arr[part2.getWidth()][part2.getHeight()] = '*';
+				int x = part2.getWidth();
+				int y = part2.getHeight();
+				arr[x][y] = '*';
 				A++;
 				B++;
 				break;
@@ -99,11 +129,31 @@ int main() {
 				part2 = part;
 			}
 		}
-		if (contourOutline(arr, 75)) {
-			system("CLS");
-			showfull();
-			cout << "Total Particles Tested: " << B << "\nTotal Particles Stuck: " << A << endl;
-			system("pause");
+		if (contourOutline(arr, 99 - xinter)) {
+			totalA += A;
+			totalB += B;
+			break;
 		}
 	}
+}
+
+int main() {
+	srand(time(NULL));
+	double porosity = 2100;
+	for (double x = 1; x < 50; ++x) {
+		double y = (porosity / (100 - x));
+		if (y == (int)y) {
+			setAgg();
+			totalA = 0;
+			totalB = 0;
+			for (int i = 0; i < 3; ++i) {
+				sim(x, y);
+				aggregate();
+				cout << " " << i;
+			}
+			write(totalA, totalB, "X" + to_string((int)x) + "Y" + to_string((int)y) + ".txt");
+			cout << endl;
+		}
+	}
+	system("pause");
 }
