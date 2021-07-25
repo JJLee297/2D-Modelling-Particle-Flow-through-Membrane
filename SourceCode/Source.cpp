5ad4214c7ef93da9c1b27f784c1d3a720241100b
Justin Lee <45504969+JJLee297@users.noreply.github.com>
2021-07-25 14:27:14 -0400

    Update Source.cpp

--- SourceCode/Source.cpp
+++ SourceCode/Source.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "RandomWalk.cpp"
-#include "contourOutline.cpp"
 #include <fstream>
 #include <stdlib.h>  
 #include <time.h>
@@ -13,15 +12,45 @@
 using namespace std;
 using ll = long long;
 
-char arr[100][200];
+// 0 is empty space, 1 is left, 2 is right
+int arr[100][200];
 int agg[100][200];
 ll totalA;
 ll totalB;
-int maxA;
-int minA;
-int maxB;
-int minB;
 
+
+bool closureCheck(int x, int y) {
+	int state;
+	if (arr[x][y] == 1)
+		state = 2;
+	else
+		state = 1;
+	if (x != 99 && y != 199 && x != 0) {
+		if (arr[x + 1][y] == state || arr[x + 1][y + 1] == state || arr[x + 1][y - 1] == state
+			|| arr[x][y + 1] == state || arr[x][y - 1] == state
+			|| arr[x - 1][y] == state || arr[x - 1][y + 1] == state || arr[x - 1][y - 1] == state)
+			return true;
+	}
+	else if (x == 99 && y != 199) {
+		if (arr[x][y + 1] == state || arr[x][y - 1] == state
+			|| arr[x - 1][y] == state || arr[x - 1][y + 1] == state || arr[x - 1][y - 1] == state)
+			return true;
+	}
+	else if (x == 0 && y != 199) {
+		if (arr[x + 1][y] == state || arr[x + 1][y + 1] == state || arr[x + 1][y - 1] == state
+			|| arr[x][y + 1] == state || arr[x][y - 1] == state)
+			return true;
+	}
+	else if (x == 99 && y == 199) {
+		if (arr[x - 1][y] == state || arr[x - 1][y - 1] == state || arr[x][y - 1] == state)
+			return true;
+	}
+	else if (x == 0 && y == 199) {
+		if (arr[x + 1][y] == state || arr[x + 1][y - 1] == state || arr[x][y - 1] == state)
+			return true;
+	}
+	return false;
+}
 
 void write(int A, int B, string name) {
 	ofstream textfile;
@@ -38,17 +67,33 @@ void write(int A, int B, string name) {
 }
 
 bool checkIfStuck(Particle part) {
-	char x = arr[part.getWidth()][part.getHeight()];
-	if (x == '*' || x == 'M' && part.getHeight() <= 185) {
+	int x = arr[part.getWidth()][part.getHeight()];
+	if ((x == 1 || x == 2) && part.getHeight() <= 185) {
 		return true;
 	}
 	return false;
 }
 
+void showfull2() {
+	for (int y = 199; y >= 0; --y) {
+		for (int x = 0; x < 100; ++x) {
+			if (arr[x][y] != 0)
+				cout << arr[x][y];
+			else
+				cout << ' ';
+		}
+		cout << endl;
+	}
+}
+
 void showfull() {
 	for (int y = 199; y >= 0; --y) {
 		for (int x = 0; x < 100; ++x) {
-			cout << arr[x][y];
+			int b = arr[x][y];
+			if (b == 1 || b == 2) 
+				cout << '*';
+			else
+				cout << ' ';
 		}
 		cout << endl;
 	}
@@ -74,7 +119,7 @@ void setAgg() {
 void aggregate() {
 	for (int x = 0; x < 100; ++x) {
 		for (int y = 0; y < 200; ++y) {
-			if (arr[x][y] == '*') {
+			if (arr[x][y] == 1 || arr[x][y] == 2) {
 				agg[x][y]++;
 			}
 		}
@@ -89,13 +134,13 @@ void createMembrane(int xx, int yy) {
 	for (int x = 0; x < 100; x++) {
 		for (int y = 0; y < 200; ++y) {
 			if (y < (slope * x + yint)) {
-				arr[x][y] = 'M';
+				arr[x][y] = 1;
 			}
 			else if (y < -slope * (x - (99 - xint))) {
-				arr[x][y] = 'M';
+				arr[x][y] = 2;
 			}
 			else {
-				arr[x][y] = ' ';
+				arr[x][y] = 0;
 			}
 		}
 	}
@@ -119,22 +164,28 @@ void sim(int xinter, int yinter) {
 				break;
 			}
 			else if (checkIfStuck(part)) {
-				arr[part2.getWidth()][part2.getHeight()] = '*';
+				if (part2.getWidth() != 0 && part2.getWidth() != 99)
+					arr[part2.getWidth()][part2.getHeight()] = arr[part.getWidth()][part.getHeight()];
+				else if (part2.getWidth() == 99)
+					arr[part2.getWidth()][part2.getHeight()] = 2;
+				else 
+					arr[part2.getWidth()][part2.getHeight()] = 1;
 				A++;
 				B++;
+				if (closureCheck(part2.getWidth(), part2.getHeight())) {
+					totalA += A;
+					totalB += B;
+					return;
+				}
 				break;
 			}
 			else {
 				part2 = part;
 			}
 		}
-		if (contourOutline(arr, 99 - xinter)) {
-			totalA += A;
-			totalB += B;
-			break;
-		}
 	}
 }
+
 
 int main() {
 	srand(time(NULL));
